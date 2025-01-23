1e53e368ad84339f7968f420f8611daeb70e681c
DmitrySopikov <autodvs2020@gmail.com>
2025-01-23 19:48:56 +0300

    tasks solved

---
+++ src/ru/javarush/python/core/level04/task13/task13.py
@@ -0,0 +1,14 @@
+# Случайная функция
+
+# Напишите функцию generate_random_number(), которая выводит на экран случайное число от -200 до 0.
+
+# Напишите тут ваш код
+import random
+
+
+def generate_random_number():
+    random_number: int = random.randint(-200, 0)
+    print(f"Сгенерированное число --> {random_number}")
+
+
+generate_random_number()
