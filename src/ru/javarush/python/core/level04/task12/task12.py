1fa859b73dc2f48beb81fcfea464c5516c423550
DmitrySopikov <autodvs2020@gmail.com>
2025-01-24 21:41:29 +0300

    tasks solved

--- src/ru/javarush/python/core/level04/task12/task12.py
+++ src/ru/javarush/python/core/level04/task12/task12.py
@@ -8,17 +8,17 @@
 # Напишите тут ваш код
 
 
-a = int(input("Введите 1е число: ") or 0)
-b = int(input("Введите 2е число: ") or 0)
-a = a if a != 0 else None
-b = b if b != 0 else None
-a = input("Введите 1е число: ") or None
-b = input("Введите 2е число: ") or None
-
-if a is None or b is None:
-    print("Сумма чисел не известна")
-else:
-    print(int(a) + int(b))
+# a = int(input("Введите 1е число: ") or 0)
+# b = int(input("Введите 2е число: ") or 0)
+# a = a if a != 0 else None
+# b = b if b != 0 else None
+# a = input("Введите 1е число: ") or None
+# b = input("Введите 2е число: ") or None
+#
+# if a is None or b is None:
+#     print("Сумма чисел не известна")
+# else:
+#     print(int(a) + int(b))
 
 a = input("Введите первое число: ")
 b = input("Введите второе число: ")
