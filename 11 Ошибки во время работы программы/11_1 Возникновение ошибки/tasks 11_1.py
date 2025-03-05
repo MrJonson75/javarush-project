de35ae5c9fb886bc412ad97858753437f7afe37e
Dmitry <165063315+MrJonson75@users.noreply.github.com>
2025-03-05 23:06:05 +0300

    Add files via upload

---
+++ 11 Ошибки во время работы программы/11_1 Возникновение ошибки/tasks 11_1.py
@@ -0,0 +1,26 @@
+# Деление на ноль.
+
+# Напиши программу, которая создает исключение ZeroDivisionError
+
+# Напишите тут ваш код
+
+
+def division(a: int, b: int) -> int:
+    return a / b
+
+
+print(division(5, 0))
+
+
+# Напиши программу, которая создает исключение TypeError
+
+# Напишите тут ваш код
+
+
+def division(a: int, b: int) -> int:
+    return a / b
+
+
+print(division(5, '5'))
+
+
